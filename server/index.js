818a1e2195725a0092c44520e69a63b3b75dc387
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-18 11:37:30 -0700

    Add basic express server to serve static html

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const servicePort = 3004;
+
+app.use(express.static('./public'));
+
+app.listen(servicePort, () => {
+  console.log('Listening to port', servicePort);
+});
