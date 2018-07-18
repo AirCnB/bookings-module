8cc6947ce22d0bc21fc2b7623c4e69649ededfaa
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-18 12:07:51 -0700

    Set up npm scripts and gitignore compiled webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
         options: {
           presets: ['env', 'react']
         }
