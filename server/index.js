76ecbe6425299b04fd1d86c7b4f0b05eb7ddf69a
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-20 14:39:06 -0700

    Serve static files for different links

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const servicePort = 3004;
 
-app.use('/', express.static('./public'));
+app.use('/aircnb/:id', express.static('./public'));
 
 app.get('/bookings/:id', (req, res) => {
   console.log(req.params);
-  res.status(200).end();
+  res.send(200).end();
 });
 
 app.listen(servicePort, () => {
