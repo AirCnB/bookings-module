27d9505353f2bfe7498373d276f7b6c2b1fef925
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-20 14:55:42 -0700

    Add ajax get function

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-      Hello from React + webpack
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.getBookingData();
+  }
+
+  getBookingData(cb) {
+    axios.get('/bookings')
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from React + webpack
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
