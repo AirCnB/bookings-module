4fcb41631fdf222a657cab877dca698fd4f51df6
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-20 12:21:15 -0700

    Get listing id from url

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 const servicePort = 3004;
 
-app.use(express.static('./public'));
+app.use('/', express.static('./public'));
+
+app.get('/bookings/:id', (req, res) => {
+  console.log(req.params);
+  res.status(200).end();
+});
 
 app.listen(servicePort, () => {
   console.log('Listening to port', servicePort);
