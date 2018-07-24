b6533d76cadfde6e31c7545ec518983283a3a038
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-24 12:24:40 -0700

    Add test for GET statusCode

--- test/test.js
+++ test/test.js
@@ -1,3 +1,19 @@
+const request = require('request');
+
+// dummy test to setup CircleCI
 test('adds 1 + 2  to equal 3', () => {
   expect(1 + 2).toBe(3);
+});
+
+// TODO: refactor to use async/await?
+// test server GET api
+test('responds to GET /listings/:id/bookings with a 200 status code', () => {
+  request.get('http://localhost:3004/listings/1/bookings')
+    .on('response', (response) => {
+      expect(response.statusCode).toBe(200);
+    })
+    .on('error', (err) => {
+      console.log(err);
+      expect(err).toBe(200);
+    });
 });
