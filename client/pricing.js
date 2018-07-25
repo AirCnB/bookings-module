9ed3ecf2ddad496b4e8e5c0a2e1da774a304eeba
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-24 17:30:42 -0700

    Dynamically render ratings count

--- client/pricing.js
+++ client/pricing.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Pricing = ({nightlyRate}) => {
+const Pricing = ({ nightlyRate, reviewAverage, reviewCount }) => {
   return (
     <div>
       <div id="nightly-rate">
         ${nightlyRate} per night
       </div>
-      <div id="average-rating">
-        ★★★★★ 200
+      <div id="rating">
+        ★★★★★ {reviewCount}
       </div>
     </div>
   );
