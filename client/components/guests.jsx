7d7a85902d83f2f5d0e683fc83860bb5721e2590
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-25 19:25:31 -0700

    Refactor component as React.Component

--- client/components/guests.jsx
+++ client/components/guests.jsx
@@ -2,13 +2,35 @@ import React from 'react';
 
 import styles from '../styles/guests.css';
 
-const Guests = () => {
+class Guests extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropDown: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.dropdown}>
+        <div id="guests-label">
+          <span className={styles.label}>Guests</span>
+        </div>
+        <button className={styles.button} type="button">1 guest</button>
+        <div className={styles.dropdownContent}>
+          <div>Adults</div>
+          <div>Children</div>
+          <div>Infants</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const ListItem = () => {
   return (
     <div>
-      <div id="guests-label">
-        <span className={styles.label}>Guests</span>
-      </div>
-      <button className={styles.button} type="button">1 guest</button>
+      adsf
     </div>
   );
 };
