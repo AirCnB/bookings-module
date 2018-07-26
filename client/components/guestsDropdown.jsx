5a14c23f7508176b1298b9511fdd644f0aee66ab
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-26 12:29:54 -0700

    Fix code style

--- client/components/guestsDropdown.jsx
+++ client/components/guestsDropdown.jsx
@@ -13,38 +13,36 @@ const GuestsDropdown = ({
   decrementInfant,
   infantCount,
   hideDropdown,
-}) => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.gridContainer}>
-        <div className={styles.left} id="left">
-          <div className={styles.title}>Adults</div>
-          <div className={styles.label}></div>
-        </div>
-        <button type="button" className={styles.button} onClick={decrementAdult}>-</button>
-        <span>{adultCount}</span>
-        <button type="button" className={styles.button} onClick={incrementAdult}>+</button>
-
-        <div className={styles.left} id="left">
-          <div className={styles.title}>Children</div>
-          <div className={styles.label}>Ages 2-12</div>
-        </div>
-        <button type="button" className={styles.button} onClick={decrementChild}>-</button>
-        <span>{childrenCount}</span>
-        <button type="button" className={styles.button} onClick={incrementChild}>+</button>
-
-        <div className={styles.left} id="left">
-          <div className={styles.title}>Infants</div>
-          <div className={styles.label}>Under 2</div>
-        </div>
-        <button type="button" className={styles.button} onClick={decrementInfant}>-</button>
-        <span>{infantCount}</span>
-        <button type="button" className={styles.button} onClick={incrementInfant}>+</button>
+}) => (
+  <div className={styles.wrapper}>
+    <div className={styles.gridContainer}>
+      <div className={styles.left} id="left">
+        <div className={styles.title}>Adults</div>
+        <div className={styles.label}></div>
       </div>
-      <div className={styles.footer}>5 guests maximum. Infants don't count toward the number of guests</div>
-      <div className={styles.textButton} onClick={hideDropdown}>Close</div>
+      <button type="button" className={styles.button} onClick={decrementAdult}>-</button>
+      <span>{adultCount}</span>
+      <button type="button" className={styles.button} onClick={incrementAdult}>+</button>
+
+      <div className={styles.left} id="left">
+        <div className={styles.title}>Children</div>
+        <div className={styles.label}>Ages 2-12</div>
+      </div>
+      <button type="button" className={styles.button} onClick={decrementChild}>-</button>
+      <span>{childrenCount}</span>
+      <button type="button" className={styles.button} onClick={incrementChild}>+</button>
+
+      <div className={styles.left} id="left">
+        <div className={styles.title}>Infants</div>
+        <div className={styles.label}>Under 2</div>
+      </div>
+      <button type="button" className={styles.button} onClick={decrementInfant}>-</button>
+      <span>{infantCount}</span>
+      <button type="button" className={styles.button} onClick={incrementInfant}>+</button>
     </div>
-  );
-};
+    <div className={styles.footer}>5 guests maximum. Infants don't count toward the number of guests</div>
+    <div className={styles.textButton} onClick={hideDropdown}>Close</div>
+  </div>
+);
 
 export default GuestsDropdown;
