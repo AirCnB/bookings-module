8532b61f03186eec974a7b0e10100e35d2e6a92b
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-26 16:37:31 -0700

    Refactor calendar as stateful component

--- client/components/dates.jsx
+++ client/components/dates.jsx
@@ -3,32 +3,41 @@ import React from 'react';
 import Calendar from './calendar.jsx';
 import styles from '../styles/dates.css';
 
-const Dates = () => {
-  return (
-    <div className={styles.container}>
-      <div id="dates-label">
-        <span className={styles.label}>Dates</span>
-      </div>
-      <div className={styles.inputContainer}>
-        <div id="checkin-box">
-          <input
-            className={styles.inputBox}
-            type="text"
-            name="checkin"
-            placeholder="Check In"
-          />
-          ➝
-          <input
-            className={styles.inputBox}
-            type="text"
-            name="checkout"
-            placeholder="Check Out"
-          />
+class Dates extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCalendar: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div id="dates-label">
+          <span className={styles.label}>Dates</span>
         </div>
+        <div className={styles.inputContainer}>
+          <div id="checkin-box">
+            <input
+              className={styles.inputBox}
+              type="text"
+              name="checkin"
+              placeholder="Check In"
+            />
+            ➝
+            <input
+              className={styles.inputBox}
+              type="text"
+              name="checkout"
+              placeholder="Check Out"
+            />
+          </div>
+        </div>
+        <Calendar />
       </div>
-      <Calendar />
-    </div>
-  );
-};
+    );
+  }
+}
 
 module.exports.Dates = Dates;
