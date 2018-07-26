40389e623ae16b245ffdec67bc48b54de424f927
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-25 19:43:37 -0700

    Toggle dropdown with clicks

--- client/components/guests.jsx
+++ client/components/guests.jsx
@@ -6,22 +6,31 @@ class Guests extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showDropDown: false,
+      showDropdown: false,
     };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown() {
+    this.setState(prevState => ({ showDropdown: !prevState.showDropdown }));
   }
 
   render() {
+    let dropdown = (
+      <div id="dropdown-content" className={styles.dropdownContent}>
+        <div>Adults</div>
+        <div>Children</div>
+        <div>Infants</div>
+      </div>
+    );
+
     return (
       <div className={styles.dropdown}>
         <div id="guests-label">
           <span className={styles.label}>Guests</span>
         </div>
-        <button className={styles.button} type="button">1 guest</button>
-        <div className={styles.dropdownContent}>
-          <div>Adults</div>
-          <div>Children</div>
-          <div>Infants</div>
-        </div>
+        <button onClick={this.toggleDropdown} className={styles.button} type="button">1 guest</button>
+        {this.state.showDropdown ? dropdown : null}
       </div>
     );
   }
