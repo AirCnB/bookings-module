243cac36019f6ed0328d7f44f484ea643c148966
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-25 18:25:03 -0700

    Style colors and size

--- client/components/guests.jsx
+++ client/components/guests.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import styles from '../styles/guests.css';
+
 const Guests = () => {
   return (
     <div>
-      <button type="button">Guests</button>
+      <div id="guests-label">
+        <span className={styles.label}>Guests</span>
+      </div>
+      <button className={styles.button} type="button">1 guest</button>
     </div>
   );
 };
