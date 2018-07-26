5c59161f4001e11703a1e2d2eb90cf6b5211d329
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-26 00:39:22 -0700

    Dynamically render guest count based on button clicks

--- client/components/guests.jsx
+++ client/components/guests.jsx
@@ -8,12 +8,23 @@ class Guests extends React.Component {
     super(props);
     this.state = {
       showDropdown: false,
+      guestCount: 1,
     };
     this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.incrementGuestCount = this.incrementGuestCount.bind(this);
+    this.decrementGuestCount = this.decrementGuestCount.bind(this);
   }
 
   toggleDropdown() {
     this.setState(prevState => ({ showDropdown: !prevState.showDropdown }));
+  }
+
+  decrementGuestCount() {
+    this.setState(prevState => ({ guestCount: prevState.guestCount - 1 }));
+  }
+
+  incrementGuestCount() {
+    this.setState(prevState => ({ guestCount: prevState.guestCount + 1 }));
   }
 
   render() {
@@ -23,7 +34,15 @@ class Guests extends React.Component {
           <span className={styles.label}>Guests</span>
         </div>
         <button onClick={this.toggleDropdown} className={styles.button} type="button">1 guest</button>
-        {this.state.showDropdown ? <GuestsDropdown /> : null}
+        {this.state.showDropdown
+          ? (
+            <GuestsDropdown
+              increment={this.incrementGuestCount}
+              decrement={this.decrementGuestCount}
+              count={this.state.guestCount}
+            />
+          )
+          : null}
       </div>
     );
   }
