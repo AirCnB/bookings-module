78c1f17c2ff11713a08edee44ec361ab5e3e59a5
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-26 19:11:45 -0700

    Add calendar header with styles

--- client/components/calendar.jsx
+++ client/components/calendar.jsx
@@ -12,6 +12,7 @@ class Calendar extends React.Component {
     this.nextMonth = this.nextMonth.bind(this);
     this.prevMonth = this.prevMonth.bind(this);
     this.monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fi', 'Sa'];
   }
 
   prevMonth() {
@@ -26,19 +27,43 @@ class Calendar extends React.Component {
     const { month } = this.state;
     return (
       <div className={styles.titleGrid}>
-        <button className={styles.button} onClick={this.prevMonth} type="button">←</button>
+        <button className={styles.button} onClick={this.prevMonth} type="button">
+          ←
+        </button>
         <span className={styles.title}>
           {month.format('MMMM YYYY')}
         </span>
-        <button className={styles.button} onClick={this.nextMonth} type="button">→</button>
+        <button className={styles.button} onClick={this.nextMonth} type="button">
+          →
+        </button>
       </div>
     );
+  }
+
+  renderHeader() {
+    return (
+      <div className={styles.header}>
+        {this.days.map((day, i) => (<span key={i}>{day}</span>))}
+      </div>
+    );
+  }
+
+  renderCalendar() {
+    const { month } = this.state;
+    const firstDayIndex = month.startOf('month').day();
+    const daysInMonth = month.daysInMonth();
+
+
+
+    console.log(firstDayIndex, daysInMonth);
   }
 
   render() {
     return (
       <div className={styles.wrapper} id="calendar">
         {this.renderTitle()}
+        {this.renderHeader()}
+        {this.renderCalendar()}
       </div>
     );
   }
