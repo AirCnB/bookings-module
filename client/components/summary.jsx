80a3453c2073409c9a57ec262a951b2d6463cf4e
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-29 13:44:25 -0700

    Statically renders summary section

--- client/components/summary.jsx
+++ client/components/summary.jsx
@@ -15,29 +15,28 @@ const renderRow = (left, right) => {
   );
 }
 
-const Summary = (props) => {
+const renderTotal = (left, right) => {
+  return (
+    <div className={styles.total}>
+      <div>
+        {left}
+      </div>
+      <div className={styles.right}>
+        {right}
+      </div>
+    </div>
+  );
+}
 
+const Summary = (props) => {
   const rate = '$149 x 7 nights';
   const total = 149 * 7;
   return (
     <div id="summary" className={styles.wrapper}>
-      {renderRow('left', 'right')}
       {renderRow(rate, `$${total.toLocaleString()}`)}
-      <div>
-        $100 x 10 nights
-      </div>
-      <div>
-        $1,000
-      </div>
-      <div>
-        Service Fee
-      </div>
-      <div>
-        Occupancy taxes and fees
-      </div>
-      <div>
-        Total
-      </div>
+      {renderRow('Service Fee', '$123')}
+      {renderRow('Occupancy Fee', '$143')}
+      {renderTotal('Total', '$1309')}
     </div>
   );
 };
