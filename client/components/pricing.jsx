b65f31adbbb9948d5edef1dc851bef8f2f179e89
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-30 15:51:12 -0700

    Dynamically render star fractions

--- client/components/pricing.jsx
+++ client/components/pricing.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 import styles from '../styles/pricing.css';
 
-const renderStars = (reviewAverage) => {
-  const numStars = Math.round(reviewAverage);
-  return '★'.repeat(numStars);
+const roundToHalf = (num) => {
+  return Math.round(num * 2) / 2;
+};
+
+const renderStars = (reviewAverage = 0) => {
+  let stars = [];
+  const TOTAL_STARS = 5;
+  const halfRound = roundToHalf(reviewAverage);
+
+  const filledCount = Math.floor(halfRound);
+  const hasHalf = halfRound % 1 !== 0;
+  const emptyCount = TOTAL_STARS - filledCount - hasHalf;
+
+  stars = stars.concat(Array(filledCount || 0).fill('star_full.svg'))
+    .concat(Array(+hasHalf || 0).fill('star_half.svg'))
+    .concat(Array(emptyCount || 0).fill('star_empty.svg'));
+
+  return stars.map(fileName => (
+    <img className={styles.stars} src={`./media/${fileName}`} />
+  ));
 };
 
 const Pricing = ({ nightlyRate, reviewAverage, reviewCount }) => {
