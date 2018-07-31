e210f927c63d41ad376f52a815081fdf09d27a90
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-30 23:09:03 -0700

    Create function to render each row

--- client/components/GuestsDropdown.jsx
+++ client/components/GuestsDropdown.jsx
@@ -2,6 +2,38 @@ import React from 'react';
 
 import styles from '../styles/guestsDropdown.css';
 
+const renderRow = (titleText, labelText, count, incrementer, decrementer) => {
+  const {
+    gridContainer,
+    left,
+    title,
+    label,
+    button,
+  } = styles;
+
+  return (
+    <div className={gridContainer}>
+      <div className={left} id="left">
+        <div className={title}>
+          {titleText}
+        </div>
+        <div className={label}>
+          {labelText}
+        </div>
+      </div>
+      <button type="button" className={button} onClick={decrementer}>
+        -
+      </button>
+      <span>
+        {count}
+      </span>
+      <button type="button" className={button} onClick={incrementer}>
+        +
+      </button>
+    </div>
+  );
+};
+
 const GuestsDropdown = ({
   decrementAdult,
   incrementAdult,
@@ -15,59 +47,9 @@ const GuestsDropdown = ({
   hideDropdown,
 }) => (
   <div className={styles.wrapper}>
-    <div className={styles.gridContainer}>
-      <div className={styles.left} id="left">
-        <div className={styles.title}>
-          Adults
-        </div>
-        <div className={styles.label}></div>
-      </div>
-      <button type="button" className={styles.button} onClick={decrementAdult}>
-        -
-      </button>
-      <span>
-        {adultCount}
-      </span>
-      <button type="button" className={styles.button} onClick={incrementAdult}>
-        +
-      </button>
-
-      <div className={styles.left} id="left">
-        <div className={styles.title}>
-          Children
-        </div>
-        <div className={styles.label}>
-          Ages 2-12
-        </div>
-      </div>
-      <button type="button" className={styles.button} onClick={decrementChild}>
-        -
-      </button>
-      <span>
-        {childrenCount}
-      </span>
-      <button type="button" className={styles.button} onClick={incrementChild}>
-        +
-      </button>
-
-      <div className={styles.left} id="left">
-        <div className={styles.title}>
-          Infants
-        </div>
-        <div className={styles.label}>
-          Under 2
-        </div>
-      </div>
-      <button type="button" className={styles.button} onClick={decrementInfant}>
-        -
-      </button>
-      <span>
-        {infantCount}
-      </span>
-      <button type="button" className={styles.button} onClick={incrementInfant}>
-        +
-      </button>
-    </div>
+    {renderRow('Adults', '', adultCount, incrementAdult, decrementAdult)}
+    {renderRow('Children', 'Ages 2-12', childrenCount, incrementChild, decrementChild)}
+    {renderRow('Infants', 'Under 2', infantCount, incrementInfant, decrementInfant)}
     <div className={styles.footer}>
       5 guests maximum. Infants don't count toward the number of guests
     </div>
