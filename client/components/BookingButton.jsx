97a0edbc0f892072e7b4bae178043d16c1d0dcd8
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-30 19:49:33 -0700

    Remove function body brackets

--- client/components/BookingButton.jsx
+++ client/components/BookingButton.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 import styles from '../styles/bookingButton.css';
 
-const BookingButton = () => {
-  return (
-    <div>
-      <button className={styles.button} type="button">
-        Request to Book
-      </button>
-      <div className={styles.label}>
-        You won&apos;t be charged yet
-      </div>
+const BookingButton = () => (
+  <div>
+    <button className={styles.button} type="button">
+      Request to Book
+    </button>
+    <div className={styles.label}>
+      You won&apos;t be charged yet
     </div>
-  );
-};
+  </div>
+);
 
 export default BookingButton;
