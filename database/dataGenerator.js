b9a38c564e3d6ccf6db95e830721d1c22e1906eb
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-01 14:53:46 -0700

    Read tsv file data

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const db = require('./index.js');
 
 // create headers
 const headers = ['id', 'nightlyRate', 'cleaningFee', 'serviceFee', 'occupancyFee', 'reviewCount', 'reviewAverage', 'reservedDates'];
@@ -30,88 +29,100 @@ const TSVRow = (id) => {
   let row = `${id}`;
 
   // add nightlyRate
-  row += getNumInRange(10, 20) * 10 + ',';
+  row += getNumInRange(10, 20) * 10 + '\t';
   // add cleaningFee
-  row += getNumInRange(5, 10) * 10 + ',';
+  row += getNumInRange(5, 10) * 10 + '\t';
   // add serviceFee
-  row += getNumInRange(10, 20) * 10 + ',';
+  row += getNumInRange(10, 20) * 10 + '\t';
   // add occupanyFee
-  row += getNumInRange(10, 20) * 10 + ',';
+  row += getNumInRange(10, 20) * 10 + '\t';
   // add reviewCount
-  row += getNumInRange(10, 100) + ',';
+  row += getNumInRange(10, 100) + '\t';
   // add reviewAverage
-  row += getNumInRange(10, 50) / 10 + ',';
+  row += getNumInRange(10, 50) / 10 + '\t';
   // add reservedDates
-  row += `"${JSON.stringify(getCalendar())}"` + ',';
-}
+  row += `"${JSON.stringify(getCalendar())}"` + '\n';
+
+  return row;
+};
 
 const createTSV = () => {
   // start TSVString with headers
   let TSVString = TSVHeader();
 
   // create each row
+  for (let id = 0; id < 99; id += 1) {
+    TSVString += TSVRow(id);
+  }
 
+  return TSVString;
 };
 
-// build the csv file
-let csvString = '';
-
-// add headers
-csvString += headers.join(',') + '\n';
-
-// create rows
-let count = 0;
-for (let i = 0; i < 100; i += 1) {
-  let row = '';
-  // add id
-  row += count++ + ',';
-  // add nightlyRate
-  row += getNumInRange(10, 20) * 10 + ',';
-  // add cleaningFee
-  row += getNumInRange(5, 10) * 10 + ',';
-  // add serviceFee
-  row += getNumInRange(10, 20) * 10 + ',';
-  // add occupanyFee
-  row += getNumInRange(10, 20) * 10 + ',';
-  // add reviewCount
-  row += getNumInRange(10, 100) + ',';
-  // add reviewAverage
-  row += getNumInRange(10, 50) / 10 + ',';
-  // add reservedDates
-  row += `"${JSON.stringify(getCalendar())}"` + ',';
-  row += '\n';
-  csvString += row;
-}
-
-// create csv file
-fs.writeFile('data.csv', csvString, (error) => {
-  if (error) {
-    console.log('fs write file error', error);
+fs.writeFile('data.tsv', createTSV(), (err) => {
+  if (err) {
+    console.log(err);
   }
 });
 
-// create an object for every row in the csv string
-const csvArray = csvString.split('\n');
-for (let row = 1; row < csvArray.length - 1; row += 1) {
-  let csvRow = csvArray[row];
-  let dataArr = csvRow.split(',');
-  let record = {};
+// // build the csv file
+// let csvString = '';
 
-  // keys for everything except the calendar
-  for (let i = 0; i < headers.length - 1; i += 1) {
-    record[headers[i]] = dataArr[i];
-  }
+// // add headers
+// csvString += headers.join(',') + '\n';
 
-  // special logic just for the calendar
-  let calendar = csvRow.slice(csvRow.indexOf('['), csvRow.length - 2);
-  let calendarArr = JSON.parse(calendar);
-  record.reservedDates = calendarArr;
+// // create rows
+// let count = 0;
+// for (let i = 0; i < 100; i += 1) {
+//   let row = '';
+//   // add id
+//   row += count++ + ',';
+//   // add nightlyRate
+//   row += getNumInRange(10, 20) * 10 + ',';
+//   // add cleaningFee
+//   row += getNumInRange(5, 10) * 10 + ',';
+//   // add serviceFee
+//   row += getNumInRange(10, 20) * 10 + ',';
+//   // add occupanyFee
+//   row += getNumInRange(10, 20) * 10 + ',';
+//   // add reviewCount
+//   row += getNumInRange(10, 100) + ',';
+//   // add reviewAverage
+//   row += getNumInRange(10, 50) / 10 + ',';
+//   // add reservedDates
+//   row += `"${JSON.stringify(getCalendar())}"` + ',';
+//   row += '\n';
+//   csvString += row;
+// }
 
-  // clear out the database and add in new data
-  let cal = new db.Booking(record);
-  cal.save((error) => {
-    if (error) {
-      console.log(error);
-    }
-  });
-}
+// // create csv file
+// fs.writeFile('data.csv', csvString, (error) => {
+//   if (error) {
+//     console.log('fs write file error', error);
+//   }
+// });
+
+// // create an object for every row in the csv string
+// const csvArray = csvString.split('\n');
+// for (let row = 1; row < csvArray.length - 1; row += 1) {
+//   let csvRow = csvArray[row];
+//   let dataArr = csvRow.split(',');
+//   let record = {};
+
+//   // keys for everything except the calendar
+//   for (let i = 0; i < headers.length - 1; i += 1) {
+//     record[headers[i]] = dataArr[i];
+//   }
+
+//   // special logic just for the calendar
+//   let calendar = csvRow.slice(csvRow.indexOf('['), csvRow.length - 2);
+//   let calendarArr = JSON.parse(calendar);
+//   record.reservedDates = calendarArr;
+
+//   // clear out the database and add in new data
+//   let cal = new db.Booking(record);
+//   cal.save((error) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//   });
+// }
