eac536a8314ee320e7b1c1083787ea14809da46c
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-01 14:53:46 -0700

    Read tsv file data

--- database/dataSeeder.js
+++ database/dataSeeder.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const db = require('./index.js');
 
 // read the tsv file
+fs.readFile('./data.tsv', (err, data) => {
+  if (err) {
+    console.log('fs write file error', err);
+  }
+  // clear out the database and add in the new data
+});
 
-// clear out the database and add in the new data
 
