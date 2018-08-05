59cdeac81239aea4b68cce659e1b1345137a0115
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-04 17:25:34 -0700

    Add tests for rendering components

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ import App from '../client/app';
 import Pricing from '../client/components/Pricing';
 import Dates from '../client/components/Dates';
 import Guests from '../client/components/Guests';
+import GuestsDropdown from '../client/components/GuestsDropdown';
 import Summary from '../client/components/Summary';
 import BookingButton from '../client/components/BookingButton';
 
@@ -25,46 +26,26 @@ describe('Test GET API', () => {
 });
 
 describe('Enzyme tests on React components', () => {
+  const reactApp = shallow(<App />);
+
   test('It should render all React components without throwing an error', () => {
-    expect(shallow(<App />).contains(<Pricing />)).toBe(true);
+    expect(reactApp.contains(<Pricing />)).toBe(true);
   });
 
   test('It should render the Dates component without throwing an error', () => {
-    expect(shallow(<App />).contains(<Dates />)).toBe(true);
+    expect(reactApp.contains(<Dates />)).toBe(true);
   });
 
   test('It should render the Guests component without throwing an error', () => {
-    expect(shallow(<App />).contains(<Guests />)).toBe(true);
+    expect(reactApp.contains(<Guests />)).toBe(true);
   });
 
   test('It should render the Summary component without throwing an error', () => {
-    expect(shallow(<App />).contains(<Summary />)).toBe(true);
+    expect(reactApp.contains(<Summary />)).toBe(true);
   });
 
   test('It should render the BookingButton component without throwing an error', () => {
-    expect(shallow(<App />).contains(<BookingButton />)).toBe(true);
+    expect(reactApp.contains(<BookingButton />)).toBe(true);
   });
 });
 
-// describe('Test the /api/listings/:id/bookings endpoint', () => {
-//   test('It should respond to the GET method with a 200 status code', (done) => {
-//     request(server)
-//       .get('/api/listings/1/bookings')
-//       .then(response => {
-//         expect(response.statusCode).toBe(200);
-//         done();
-//       })
-//       .catch(err => {
-//         done();
-//       });
-//   });
-// });
-
-// describe('Test the /api/listings/:id/bookings endpoint', () => {
-//   test('It should respond to the GET method with a 200 status code', () => {
-//     return request(app).get('/api/listings/1/bookings').then(response => {
-//       expect(response.statusCode).toBe(200);
-//       server.close();
-//     });
-//   });
-// });
