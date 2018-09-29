8989d29a06a2c3728e7e207f81a2ddcd25f8d29f
edwardbaeg <edwardbaeg9@gmail.com>
2018-09-28 17:46:25 -0700

    Test for response type

--- test/api.test.js
+++ test/api.test.js
@@ -3,14 +3,20 @@ const app = require('../server/index.js');
 
 describe('Test GET API', () => {
   test('It should respond to GET /api/listings/0/bookings with a 200 statusCode', () => {
-    return request(app).get('/api/listings/1/bookings').then(response => {
+    return request(app).get('/api/listings/1/bookings').then((response) => {
       expect(response.statusCode).toBe(200);
     });
   });
 
   test('It should respond to GET /api/listings/200/bookings with a 404 statusCode', () => {
-    return request(app).get('/api/listings/200/bookings').then(response => {
+    return request(app).get('/api/listings/200/bookings').then((response) => {
       expect(response.statusCode).toBe(404);
+    });
+  });
+
+  test('It should respond to GET with a json object', () => {
+    return request(app).get('/api/listings/1/bookings').then((reponse) => {
+      expect(reponse.type).toBe('application/json');
     });
   });
 });
